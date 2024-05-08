af0f7d1b192876ca336084e6c9c4d5bf734f8611
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 21:42:20 +0300

    update font family and change text header and home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="h-full flex justify-center items-center bg-purple-700">
-      <div className="space-y-6 text-center">
+      <div className="flex flex-col justify-center items-center gap-y-5 text-center">
         <h1 className="text-6xl font-semibold text-white tracking-wider">
-          Auth
+          HB3S 🚀
         </h1>
         <p className="text-xl font-medium text-white">
-          A simple authentication server
+          Measuring Blood Glucose by A Non-Invasive method
         </p>
         <LoginButton>
           <Button variant="outline">Sign in</Button>
