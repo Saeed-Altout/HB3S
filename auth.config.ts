5a43b4e1c653c28514a14d64d2bfec865fde5dff
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 14:56:12 +0300

    update login action and next config file

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,32 @@
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+import { loginSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export default {
-  providers: [],
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      type: "credentials",
+      credentials: {},
+      async authorize(credentials) {
+        const validateFields = loginSchema.safeParse(credentials);
+
+        if (!validateFields.success) return null;
+
+        const { email, password } = validateFields.data;
+
+        const user = await getUserByEmail(email);
+        if (!user || !user.password) return null;
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) return null;
+
+        return user;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
