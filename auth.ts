1f2948bbc704ceb7cdef402674f8d34d6e18c2eb
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 15:22:40 +0300

    reflect token to session as id for user

--- auth.ts
+++ auth.ts
@@ -5,6 +5,15 @@ import { db } from "./lib/db";
 import authConfig from "@/auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
