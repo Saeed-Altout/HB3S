fe1e1255f00c976a005ca1bde4d9b0ad17bd3ce8
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 13:19:15 +0300

    install install bcryptjs and @types/bcryptjs libs, create first user by register form

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,39 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+
+import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validatedFields = registerSchema.safeParse(values);
+
   if (!validatedFields.success) {
     return { error: "Invalid Fields!" };
   }
-  const { name, email, password } = validatedFields.data;
 
-  return { success: "Email Sent" };
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification token email
+
+  return { success: "User created" };
 };
