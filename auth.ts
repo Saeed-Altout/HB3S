d08977b34cb6d67750aca33ce0a6bac8ef3fcc5a
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 16:54:10 +0300

    create error page, error card component, added path to routes config and update auth config by added pages route

--- auth.ts
+++ auth.ts
@@ -7,6 +7,11 @@ import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+
   events: {
     async linkAccount({ user }) {
       await db.user.update({
