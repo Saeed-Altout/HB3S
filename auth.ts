b5d5b976f69516b15ccfcfc5da3532a894009f6e
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 15:56:42 +0300

    update schema prisma, added UseRole model and fix type for this action and reflect role to session as role for user

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 
-import { db } from "./lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
+import { db } from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
@@ -11,7 +13,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         session.user.id = token.sub;
       }
 
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
     },
   },
   adapter: PrismaAdapter(db),
