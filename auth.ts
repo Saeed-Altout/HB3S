9b2d86d0425f4be3d0ce27a0ce11f5192836f917
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 16:40:49 +0300

    update info user when sign in by use providers

--- auth.ts
+++ auth.ts
@@ -7,6 +7,15 @@ import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
+
   callbacks: {
     async session({ session, token }) {
       if (session.user && token.sub) {
