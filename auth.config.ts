788de1f33594b871b7e99dd111fc6a3fa474253f
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-08 16:25:54 +0300

    added Google provider, client id and client secret

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,7 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 
 import { loginSchema } from "@/schemas";
@@ -11,6 +12,10 @@ export default {
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     Credentials({
       id: "credentials",
