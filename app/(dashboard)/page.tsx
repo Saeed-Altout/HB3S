ee76c0b7e3f2803d73143b671a2198d5b03814e2
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-14 00:49:23 +0300

    change content page from dashboard to root page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useCreatePerson } from "@/hooks/use-create-person";
+
 export default function DashboardPage() {
+  const isOpen = useCreatePerson((state) => state.isOpen);
+  const onOpen = useCreatePerson((state) => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
   return <div>Dashboard page</div>;
 }
