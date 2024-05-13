b1249a4189a9f373f1253dc8dd3d7ede77d1e30e
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-14 00:44:51 +0300

    delete marketing and added accordion component and create auth lib

--- app/(marketing)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "./_components/header";
-
-export default function MarketingPage() {
-  return (
-    <div className="flex items-center justify-center flex-col container mx-auto">
-      <Header />
-    </div>
-  );
-}
